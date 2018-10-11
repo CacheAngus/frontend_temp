be96c0c79d78857f06189d45e98ecb1a19e04998
Nicholas Chan <nicholasdlchan60@gmail.com>
2018-10-10 22:53:21 -0400

    UserTable Component
    Completed the component for the user Table to view certificates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import InstitutionTable from './components/InstitutionTable/InstitutionTable';
+import UserTable from './components/UserTable/UserTable';
 class App extends Component {
   constructor(props) {
         super(props);
