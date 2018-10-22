7dc91322f44fc582e026fa8bacafa0abc5613c43
Cache Angus <15sa54@caslab.queensu.ca>
2018-10-21 21:06:36 -0400

    trying to seperate components and add int axios api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import LastName from './certificatePage.js';
+import FirstName from './certificatePage.js';
+import Notes from './certificatePage.js';
+import Certificate from './certificatePage.js';
+import EthAddress from './certificatePage.js';
+
+const axios = require('axios');
 
 class App extends Component {
   constructor(props) {
@@ -17,179 +24,57 @@ handleSubmit(e){
     return;
   }
   alert('Form Submitted');
+  //does this even go in here idk, probably should add in error catching
+  axios({
+    method: 'post',
+    //figure out where to actually post the info
+    url: '/posting',
+    data: {
+      lastNameValue: this.state.lastNameValue,
+      firstNameValue: this.state.firstNameValue,
+      ethAddressValue: this.state.ethAddressValue,
+      certificateValue: this.state.certificateValue,
+      notesValue: this.state.notesValue
+       }
+  });
   e.preventDefault();
-  alert('Form Submitted!')
+  
+
 };
   
 
 
   render() {
     return (
-      <div className="App" style={this.state.appBackground}>
+      <div className="App" style={style.row}>
+      <view style={style.row}>
                 <form onSubmit={this.handleSubmit}>
-                  <label id="certificate">
+                  <label id="title">
                     Create Certificate
-                    {/*textare value = this.state and then all of the component*/}
-                   <view style={style.row}>
+                    
+                   
                     <FirstName />
                     <LastName /> 
-                    </view>
+                   
                     <Certificate />
                     <br/>
                     <Notes />
                     <EthAddress/>
                     <br/>
-                   {/*} <label>
-                      Add Certificate
-                    <button type="button" onClick={this.handleClicked}>Add Cerificate</button>
-    </label>*/}
-                </label>  
-                <input type = "submit" name="certificate"  value="Create Certificate" />
+                    </label>  
+                <input type = "submit" id="certificate"  value="Create Certificate" />
                 </form>
-
+                </view>
               </div>
     );
     } ;
-
-
 };
 
-class LastName extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lastNameValue: "",
-    };
 
-    this.changeLastName = this.changeLastName.bind(this);
-  }
 
-changeLastName(e) {
-this.setState({lastNameValue: e.target.value});
-};
-render() {
-return (
-  <div className="LastName">
-            <div style={this.state.formStyle}>
-              <label>
-                Last Name:
-            </label>
-            <input type="text" name="lastname" value={this.state.lastNameValue} onChange={this.changeLastName} required/>
-            </div>
+//trying to post the information somewhere
 
-          </div>
-);
-}
-}
-class EthAddress extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      ethAddressValue: "",
-    };
 
-    this.changeEthAddress = this.changeEthAddress.bind(this);
-  }
-
-changeEthAddress(e) {
-this.setState({ethAddressValue: e.target.value});
-}
-render() {
-return (
-  <div className="EthAddress">
-            <div style={this.state.formStyle}>
-              <label>
-                EthAddress:
-                </label>
-                <input type="text" name="ethadd" value={this.state.ethAddressValue} onChange={this.changeEthAddress} required/>
-            </div>
-
-          </div>
-);
-};
-};
-
-class FirstName extends Component{
-  constructor(props) {
-        super(props);
-        this.state = {
-            firstNameValue: "",
-        };
-        this.changeFirstName = this.changeFirstName.bind(this);
-    }
-
-  changeFirstName(e) {
-    this.setState({firstNameValue: e.target.value});
-}
-render() {
-  return (
-            <div>
-              <label>
-                  First Name:
-                  </label>
-                  <input  type="text" name="firstname" value={this.state.firstNameValue} onChange={this.changeFirstName} required/>
-            </div>
-  );
-};
-
-};
-
-class Certificate extends Component{
-  constructor(props) {
-        super(props);
-        this.state = {
-            certificateValue: "",
-        };
-        this.changeCertificate = this.changeCertificate.bind(this);
-    }
-
-  changeCertificate(e) {
-    this.setState({certificateValue: e.target.value});
-};
-render() {
-  return (
-            <div>
-              <label>
-                  Certificate:
-                  <select>
-                  <option value="none">  </option>
-                    <option value="bachelor">Bachelor Degree</option>
-                    <option value="course">Course</option>
-                    <option value="govid">Government ID</option>
-                  </select>
-                  </label>
-                 
-            </div>
-  );
-};
-
-};
-
-class Notes extends Component{
-  constructor(props) {
-        super(props);
-        this.state = {
-            notesValue: "",
-        };
-        this.changeNotes = this.changeNotes.bind(this);
-    }
-
-  changeNotes(e) {
-    this.setState({notesValue: e.target.value});
-};
-render() {
-  return (
-            <div>
-              <label>
-                  Notes:
-                  </label>
-                  <input type="text" name = "notes" value={this.state.notesValue} onChange={this.changeNotes}/>
-            </div>
-  );
-};
-
-};
- 
 let style ={
   overallForm: {
    backgroundColor: 'grey',
