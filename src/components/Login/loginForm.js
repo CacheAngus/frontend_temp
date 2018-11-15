50a07156dcec9dfd0dad40ec9cd4feda4cf81d6f
Harry <chanharryhk@gmail.com>
2018-11-14 22:43:14 -0500

    Completed facebook login

--- src/components/Login/loginForm.js
+++ src/components/Login/loginForm.js
@@ -1,49 +1,93 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, TextInput, TouchableOpacity, Text} from 'react-native';
+// import {StyleSheet, View, TextInput, TouchableOpacity, Text} from 'react-native';
+import FacebookLogin from 'react-facebook-login';
+import './loginForm.css';
 
 class LoginForm extends Component {
+    state = {
+      isLoggedIn: false,
+      userID: "",
+      name: "",
+      email: "",
+      picture: ""
+    }
+    responseFacebook = response => {
+      this.setState({
+        isLoggedIn: true,
+        userID: response.userID,
+        name: response.name,
+        email: response.email,
+        picture: response.picture.data.url
+      })
+      console.log(response);
+    }
+    componentClicked = () => console.log("Clicked!");
     render() {
+        let fbContent;
+
+        if (this.state.isLoggedIn) {
+          fbContent = (
+            <div>
+              <img src={this.state.picture} alt={this.state.name}/>
+            </div>
+          );
+        }
+        else{
+          fbContent = (
+            <FacebookLogin
+            appId="189338485307527"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={this.componentClicked}
+            callback={this.responseFacebook} />
+          );
+        }
         return (
-            <View style={styles.container}>
-            <TextInput
-            placeholder="username"
-            placeholderTextColor="rgb(48,48,48)"
-            style={styles.input}
-            />
-            <TextInput
-            placeholder="password"
-            placeholderTextColor="rgb(48,48,48)"
-            secureTextEntry
-            style={styles.input}
-            />
-            <TouchableOpacity style={styles.buttonContainer}>
-                <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-            </View>
+            <div className="login">
+              <h1>Log In</h1>
+              {fbContent}
+            </div>
+            // <View style={styles.container}>
+            // <TextInput
+            // placeholder="username"
+            // placeholderTextColor="rgb(48,48,48)"
+            // style={styles.input}
+            // />
+            // <TextInput
+            // placeholder="password"
+            // placeholderTextColor="rgb(48,48,48)"
+            // secureTextEntry
+            // style={styles.input}
+            // />
+            //
+            // <TouchableOpacity style={styles.buttonContainer}>
+            //     <Text style={styles.buttonText}>Login</Text>
+            // </TouchableOpacity>
+            // </View>
         );
     }
 }
 
 export default LoginForm
 
-const styles = StyleSheet.create({
-    container: {
-        padding:20
-    },
-    input: {
-        height: 40,
-        backgroundColor: 'rgb(245,245,245)',
-        marginBottom: 20,
-        paddingHorizontal: 10
-
-    },
-    buttonContainer: {
-        backgroundColor: 'rgb(128,128,128)',
-        paddingVertical: 15
-    },
-buttonText: {
-    textAlign: 'center',
-    color: '#00000',
-    fontWeight: '700'
-}
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         padding:20
+//     },
+//     input: {
+//         height: 40,
+//         backgroundColor: 'rgb(245,245,245)',
+//         marginBottom: 20,
+//         paddingHorizontal: 10
+//
+//     },
+//     buttonContainer: {
+//         backgroundColor: 'rgb(128,128,128)',
+//         paddingVertical: 15
+//     },
+// buttonText: {
+//     textAlign: 'center',
+//     color: '#00000',
+//     fontWeight: '700'
+// }
+// });
