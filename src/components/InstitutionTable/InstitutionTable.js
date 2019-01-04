63052c22a576fc46811008f6080e858260d577d5
Jamie Linsdell <jamie.linsdell@gmail.com>
2019-01-04 13:50:10 -0700

    Business CRUD, need to implement into the presentation of the table.

--- src/components/InstitutionTable/InstitutionTable.js
+++ src/components/InstitutionTable/InstitutionTable.js
@@ -8,6 +8,70 @@ import "react-table/react-table.css";
 //import SortableTbl from "react-sort-search-table";
 import FilterableTable from "react-filterable-table"
 //gotta do the acios thing when we get the api
+/*const FilterableTable = require('react-filterable-table');
+
+class InstitutionTable extends Component {
+
+
+  constructor(props) {
+    super();
+    this.state = {
+
+      data: [
+        {
+            certificateNo: 1,
+            firstName: 'Nicholas',
+            lastName: 'Chan',
+            certificate: 'Bachelor of Commerce',
+            notes: 'this guy is a g',
+            ethAdd: "1234"
+      },
+      {
+        certificateNo: 2,
+        firstName: 'cahe',
+        lastName: 'angus',
+        certificate: 'Bachelor of Commerce',
+        notes: 'nope',
+        ethAdd: "1234"
+  },
+    ]
+    };
+
+  }
+
+
+  render() {
+    const { data } = this.state;
+    return (
+
+        <SortableTbl
+          tblData={data}
+          tHead = {tHead}
+          dKey={col}
+          />
+    );
+  }
+}
+
+
+let col = [
+  "firstName",
+  "lastName",
+  "certificate",
+  "notes",
+  "ethAdd"
+];
+let tHead = [
+  "First Name",
+  "Last Name",
+  "Certificate",
+  "Notes",
+  "Etherium Address"
+];
+*/
+
+
+//other filterable table
 
 class InstitutionTable2 extends Component {
 
