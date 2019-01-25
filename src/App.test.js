4003d1ae2bb134738622ffc39088a3497e7425e2
Cache Angus <15sa54@caslab.queensu.ca>
2019-01-24 21:09:00 -0500

    Nav bar

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CompanyLogin from './loginCompany';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import "react-table/react-table.css";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CompanyLogin />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import CompanyLogin from './components/Login/loginCompany';
+import UserTable from './components/UserTable/UserTable';
+
+// Navigation
+import NavigationBar from './components/Navigation/navBar.js';
+import Home from './components/Home/home.js';
+import { Router, Route, IndexRoute} from 'react-router'
+import { BrowserRouter} from 'react-router-dom'
+
+// Login Flow
+import firebase from 'firebase'
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstNameValue: "",
+      lastNameValue: "",
+      certificateValue: "",
+      notesValue: "",
+      // isSignedIn: false,
+    };
+    firebase.database().ref('users/' + '1234').set({
+      username: "testing", //test
+      email: "test",
+      profile_picture : "test"
+    });
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
+
+handleSubmit(e){
+  if (!e.target.checkValidity()) {
+    // form is invalid! so we do nothing
+    return;
+  }
+  alert('Form Submitted');
+  //does this even go in here idk, probably should add in error catching
+  axios({
+    method: 'post',
+    //figure out where to actually post the info and how to post it
+    url: './posting',
+    data: {
+      lastNameValue: this.state.lastNameValue,
+      firstNameValue: this.state.firstNameValue,
+      ethAddressValue: this.state.ethAddressValue,
+      certificateValue: this.state.certificateValue,
+      notesValue: this.state.notesValue
+       }
+  });
+  e.preventDefault();
+  alert('Form Submitted!')
+};
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className={`App__Container`}>
+          <NavigationBar />
+            {/* <div className="App" style={this.state.appBackground}>*/}
+            <Route path="/" component={Home} exact/>
+            <Route path="/view" name="view" component={UserTable} exact/>
+
+            {this.state.isSignedIn ? (
+              <span>
+                <div>Signed In!</div>
+                <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+                <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+                <img
+                  alt="profile picture"
+                  src={firebase.auth().currentUser.photoURL}
+                />
+              </span>
+            ) : (
+              <StyledFirebaseAuth
+                uiConfig={this.uiConfig}
+                firebaseAuth={firebase.auth()}
+              />
+            )}
+      {/*   </div> */}
+      </div>
+    </BrowserRouter>
+  )};
+};
+
+export default App;
--- src/components/Navigation/navBar.js
+++ src/components/Navigation/navBar.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import './navBar.css'
+import {MenuItem, Navbar, Nav, NavItem, NavDropdown} from 'react-bootstrap';
 // style={{ textDecoration: 'none', paddingRight:'220px', color:'black'}}>
 
-class NavigationBar extends Component{
+/*class NavigationBar extends Component{
   render(){
     return(
       <div className={`NavigationBar`}>
@@ -15,6 +16,46 @@ class NavigationBar extends Component{
         </div>
       </div>
   )}
+}
+ */
+class NavigationBar extends Component {
+  render(){
+    return(
+  
+<Navbar inverse collapseOnSelect>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#brand">React-Bootstrap</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavItem eventKey={1} href="#">
+        Link
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Link
+      </NavItem>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+    <Nav pullRight>
+      <NavItem eventKey={1} href="#">
+        Link Right
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Link Right
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    )}
 }
 
 export default NavigationBar
