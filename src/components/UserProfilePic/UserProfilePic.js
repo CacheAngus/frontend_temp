4b7af98e2453451b52701a00c4f447c6899ef21f
Cache Angus <15sa54@caslab.queensu.ca>
2019-03-18 22:15:14 -0400

    These are all the changes that the email prof thing should have

--- src/components/UserProfilePic/UserProfilePic.js
+++ src/components/UserProfilePic/UserProfilePic.js
@@ -4,19 +4,39 @@ import './userProfilePic.css'
 import { Image, Button} from 'react-bootstrap';
 import {AuthenticationConsumer} from '../AuthenticationContext/AuthenticationContext'
 // style={{ textDecoration: 'none', paddingRight:'220px', color:'black'}}>
+import firebase from 'firebase';
 
 class UserProfilePic extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
+   this.state={
+     
+   } 
+   var rootRef = firebase.database().ref().child('users'); 
+   rootRef.on("child_added", snap=> {   
+      if(this.props.authUser.email === snap.child('email').val()){
+        
+        var photoURL;
+        var users = firebase.auth().currentUser;
+          photoURL = users.photoURL;
+          console.log(photoURL);
+          if(photoURL != null){   
+             users.updateProfile({
+               photoURL: snap.child('picture').val()
+             })
+          }
+        }
+  });
   }
   render(){
+    
     return(
       <AuthenticationConsumer>
         {(auth) => (
           <div>
             <img
-           className="rounded-circle test" alt="profile picture"
-              src={auth.authUser ? auth.authUser.photoURL : ""}/>
+className="rounded-circle test" alt="profile picture"
+    src= {auth.authUser  ? auth.authUser.photoURL : "" }/>
           </div>
         )}
 
