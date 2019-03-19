e309819d54a1a6f2b1dac12c25c703b5fc505c93
Cache Angus <15sa54@caslab.queensu.ca>
2019-03-19 18:18:07 -0400

    Tables filtering properly

--- src/components/UserTable/UserTable.js
+++ src/components/UserTable/UserTable.js
@@ -20,15 +20,25 @@ class UserTable extends Component {
     this.state = {
       data: [
         {
+            firstName: "",
+            lastName: "",
+            busmail: "",
+            email: '',
+            certificate: '',
+            notes: ''
       }
     ],
-    modalShow: false
+    modalShow: false,
+    selected: {
+      name: "hi",
+      desc: "this is the desc"
+    }
   };
   }
-
   componentDidMount(){
-    /* var businesses;
-    var emailName; */
+    this.getData();
+    var businesses;
+    var emailName;
     //console.log(this.props);
     var rootRef = firebase.database().ref().child('certificates');
     rootRef.on("child_added", snap=> {
@@ -40,12 +50,18 @@ class UserTable extends Component {
       var bus_email = snap.child("busmail").val();
       var business = snap.child("business").val();
       var courseDes = snap.child('descr').val();
-
-
+      var view = "View";
+      var obj = { firstName: fname,
+                  lastName: lname,
+                  busmail: business,
+                  email: us_email,
+                  certificate: cert,
+                  notes: note};
+      //check what the type is on the user
       if(firebase.database().ref().child("business").orderByChild("email").equalTo(this.props.authUser.email).once("value", snapshot=> { 
         if(snapshot.exists()){
           if(this.props.authUser.email===bus_email)
-          $("#data-table").append("<tr><td>" + fname + "</td><td>" + lname +"</td><td>" + cert +"</td><td>" + business + "</td><td>" + us_email + "</td><td>" + note + "</td><td>" + courseDes +"</td></tr>" );
+          this.state.data.push(obj);
         return true;  
         }
       })===true){
@@ -53,7 +69,7 @@ class UserTable extends Component {
       }  else if (firebase.database().ref().child("users").orderByChild("email").equalTo(this.props.authUser.email).once("value", snapshot=> {   
         if(snapshot.exists()){
           if(this.props.authUser.email===us_email)
-          $("#data-table").append("<tr><td>" + fname + "</td><td>" + lname +"</td><td>" + cert +"</td><td>" + business + "</td><td>" + us_email + "</td><td>" + note + "</td><td>" + courseDes +"</td></tr>" );
+          this.state.data.push(obj);
         return true; 
         }
       })===true){
@@ -65,40 +81,39 @@ class UserTable extends Component {
 
       
      });
-      
      
-      
   }
-
+  getData(){
+    var refreshData = this.state.data;
+    this.setState({data: refreshData});
+    console.log("getData");
+    }
   render() {
     let modalClose = () => this.setState({modalShow: false});
 
-
-  console.log(this.props);
+    console.log("our data");
+    console.log(this.state);
+    console.log("our props");
+    console.log(this.props);
     //const {columns} = this.state.column s;
+
       return (
-      
+
         <div>
-         <UserModal authUser={this.props.authUser} show={this.state.modalShow} onHide={modalClose}/>
+         <UserModal authUser={this.props.authUser} show={this.state.modalShow} onHide={modalClose} name={this.state.selected.name} coursedesc={this.state.selected.desc}/>
         <div id="create-page">
-          <UserPageBanner authUser={this.props.authUser}/>
-
-
-
-
-
+          <UserPageBanner authUser={this.props.authUser} />
          <div className="container" >
 
            <Nav id="navTabs" pullRight variant="tabs" style={{paddingTop: 50}}>
-            <NavItem class='navItem'>
-               <Link to="/view"> Recent</Link>
-            </NavItem>
-             <NavItem class='navItem'>
-                <button type="button" id='' class="btn-link" data-toggle="modal" data-target="#exampleModal" >Course</button>
-                <UserModal show={this.state.modalShow} onHide={modalClose}/>
+             <NavItem>
+               <Link to="/view"> Recent
+             </Link></NavItem>
+             <NavItem style={{marginLeft:25}}>
+               Business
              </NavItem>
 
-             <NavItem class='navItem'>
+             <NavItem style={{marginLeft:25}}>
                <Link to="/certificate">Certificate</Link>
            </NavItem>
            </Nav>
@@ -112,41 +127,28 @@ class UserTable extends Component {
                <th>Email</th>
                <th>Course Name</th>
                <th>Description</th>
+               <th>View</th>
              </tr>
            </thead>
            <tbody>
-
+           {this.state.data.map((r,i) => (
+                     <tr key={i}>
+                         <td>{r.firstName}</td>
+                         <td>{r.lastName}</td>
+                         <td>{r.certificate}</td>
+                         <td>{r.busmail}</td>
+                         <td>{r.email}</td>
+                         <td>{r.notes}</td>
+                         <td></td>
+                         <td> <button type='button' class='btn btn-link' data-toggle='modal' data-target='#exampleModal' onClick={() => this.setState({ modalShow: true, selected: {name: r.certificate, desc: r.notes}})}> View </button> </td>
+                     </tr>
+                   ))}
            </tbody>
            </Table>
 
       </div>
       </div>
     </div>
-
-
-      //   <ReactTable
-      //     data={data}
-      //     columns={[{
-      //         Header: 'Certificate No',
-      //         accessor: 'certificateNo' // String-based value accessors!
-      //       },
-      //       {
-      //         Header: 'Certificate',
-      //         accessor: 'certificate'
-      //       },
-      //       {
-      //         Header: 'Date Issued',
-      //         accessor: 'date'
-      //       },
-      //       {
-      //         Header: 'Notes',
-      //         accessor: 'notes'
-      //       }]}
-      //     defaultPageSize={10}
-      //     className="-striped -highlight"
-      //   />
-      //   <br />
-      // </div>
     );
   }
 }
